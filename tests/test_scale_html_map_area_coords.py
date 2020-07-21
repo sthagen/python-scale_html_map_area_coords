717982f3ec69d88e1132c00631bfb61fe3f880f6
Stefan Hagen <stefan@hagen.link>
2020-07-21 13:18:00 +0200

    Update test_scale_html_map_area_coords.py

--- tests/test_scale_html_map_area_coords.py
+++ tests/test_scale_html_map_area_coords.py
@@ -13,7 +13,13 @@ def test_apply_scaling_ok_empty_string():
     assert do.apply_scaling('') == ''
 
 
-def test_apply_scaling_nok_ints():
+def test_apply_scaling_nok_list_of_ints():
     message = r"'list' object has no attribute 'rstrip'"
     with pytest.raises(AttributeError, match=message):
         do.apply_scaling([1, 2, 3])
+
+
+def test_apply_scaling_nok_int():
+    message = r"'int' object has no attribute 'rstrip'"
+    with pytest.raises(AttributeError, match=message):
+        do.apply_scaling(42)
