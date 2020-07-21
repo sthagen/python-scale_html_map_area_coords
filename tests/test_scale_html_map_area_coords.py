766ab1ba7766a0e23bc7c798ab2918f166dd57f2
Stefan Hagen <stefan@hagen.link>
2020-07-21 13:40:32 +0200

    Updated signatures for calls in tests

--- tests/test_scale_html_map_area_coords.py
+++ tests/test_scale_html_map_area_coords.py
@@ -7,23 +7,23 @@ import scale_html_map_area_coords.scale_html_map_area_coords as do
 
 
 def test_apply_scaling_ok_string():
-    assert do.apply_scaling("imension is implicit") == "imension is implicit"
+    assert do.apply_scaling(2, "imension is implicit") == "imension is implicit"
 
 
 def test_apply_scaling_ok_empty_string():
-    assert do.apply_scaling('') == ''
+    assert do.apply_scaling(2, '') == ''
 
 
 def test_apply_scaling_nok_list_of_ints():
     message = r"'list' object has no attribute 'rstrip'"
     with pytest.raises(AttributeError, match=message):
-        do.apply_scaling([1, 2, 3])
+        do.apply_scaling(2, [1, 2, 3])
 
 
 def test_apply_scaling_nok_int():
     message = r"'int' object has no attribute 'rstrip'"
     with pytest.raises(AttributeError, match=message):
-        do.apply_scaling(42)
+        do.apply_scaling(2, 42)
 
 
 def test_scale_html_map_area_coords_nok_reduction_gibven_but_empty_path():
