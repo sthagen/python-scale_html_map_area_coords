f43927a287a6316473a5f466e2be325b6d9081d7
Stefan Hagen <stefan@hagen.link>
2020-07-21 13:11:04 +0200

    Added initial test case for single string

---
+++ tests/test_scale_html_map_area_coords.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=missing-docstring,unused-import,reimported
+import pytest  # type: ignore
+
+import scale_html_map_area_coords.scale_html_map_area_coords as do
+
+
+def test_main_ok_string():
+    assert do.apply_scaling("imension is implicit") == "imension is implicit\n"
