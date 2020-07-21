f53156e150f6d07221c2f4592220cad1a693e71f
Stefan Hagen <stefan@hagen.link>
2020-07-21 13:13:41 +0200

    Added empty string test

--- tests/test_scale_html_map_area_coords.py
+++ tests/test_scale_html_map_area_coords.py
@@ -7,3 +7,7 @@ import scale_html_map_area_coords.scale_html_map_area_coords as do
 
 def test_main_ok_string():
     assert do.apply_scaling("imension is implicit") == "imension is implicit"
+
+
+def test_main_ok_empty_string():
+    assert do.apply_scaling('') == ''
