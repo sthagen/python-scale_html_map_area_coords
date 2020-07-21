da0a2165821400585991bac3fd6f7a396db28c1f
Stefan Hagen <stefan@hagen.link>
2020-07-21 15:17:03 +0200

    Fixed test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -19,7 +19,7 @@ def test_main_nok_reduction_given_but_non_existing_path():
 def test_main_nok_reduction_wrong_type_and_non_existing_path():
     nef = non_existing_file_path = 'file_does_not_exist'
     assert pathlib.Path(nef).is_file() is False, f"Unexpected file {nef} exists which may break other tests"
-    message = f"unsupported operand type\(s\) for //: 'int' and 'NoneType'"
+    message = f"int\(\) argument must be a string, a bytes-like object or a number, not 'NoneType'"
     with pytest.raises(TypeError, match=message):
         cli.main([None, non_existing_file_path])
 
