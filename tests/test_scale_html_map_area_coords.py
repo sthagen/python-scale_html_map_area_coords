ffabcb977419ad822e6a99cae43edb678e4cf984
Stefan Hagen <stefan@hagen.link>
2020-07-21 13:12:11 +0200

    Fixed test expectation

--- tests/test_scale_html_map_area_coords.py
+++ tests/test_scale_html_map_area_coords.py
@@ -6,4 +6,4 @@ import scale_html_map_area_coords.scale_html_map_area_coords as do
 
 
 def test_main_ok_string():
-    assert do.apply_scaling("imension is implicit") == "imension is implicit\n"
+    assert do.apply_scaling("imension is implicit") == "imension is implicit"
